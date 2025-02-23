5056b209b1af7c5af7b4bafc4a8817e7a47e24af
roy-subhro <subhro.roy98@gmail.com>
2025-02-23 19:36:16 +0530

    Flask App

---
+++ app.py
@@ -0,0 +1,13 @@
+
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/accept',methods=['POST'])
+def anyfunc():
+    data = request.get_json()
+    num1 = data.get('a')
+    num2 = data.get('b')
+    return jsonify({'sum': num1+num2})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
